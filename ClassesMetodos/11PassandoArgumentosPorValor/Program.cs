3a18b0a13916fed2954def6f530967c9f49f3f53
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-10 15:59:13 -0300

    Argumento por valor

---
+++ ClassesMetodos/11PassandoArgumentosPorValor/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("## PASSANDO ARGUMENTOS POR VALOR ##\n");
+
+int x = 20;
+
+Console.WriteLine("\n Valor do argumento x antes de passar por valor: " + x);
+
+Calculo calc = new Calculo();
+calc.Dobrar(x);
+
+Console.WriteLine("\n Valor do Argumento x depois de passar por valor " + x);
+
+Console.ReadKey();
+
+
+public class Calculo
+{
+    public void Dobrar(int y)
+    {
+        y *= 2;
+
+        Console.WriteLine("\n Valor do parâmetro Y no método Dobrar: " + y);
+    }
+}
+    
