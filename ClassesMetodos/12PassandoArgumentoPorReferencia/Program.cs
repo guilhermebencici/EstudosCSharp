d0fa493f12874145fe36322540613e0deb5549d9
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-10 16:07:46 -0300

    Passando argumento por referencia REF

---
+++ ClassesMetodos/12PassandoArgumentoPorReferencia/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## PASSANDO ARGUMENTOS POR REFERENCIA ##\n");
+
+int x = 20;
+
+Console.WriteLine("\n Valor do argumento x antes de passar por valor: " + x);
+
+Calculo calc = new Calculo();
+calc.Dobrar(ref x);
+
+Console.WriteLine("\n Valor do Argumento x depois de passar por valor " + x);
+
+Console.ReadKey();
+
+
+public class Calculo
+{
+    public void Dobrar( ref int y)
+    {
+        y *= 2;
+
+        Console.WriteLine("\n Valor do parâmetro Y no método Dobrar: " + y);
+    }
+}
