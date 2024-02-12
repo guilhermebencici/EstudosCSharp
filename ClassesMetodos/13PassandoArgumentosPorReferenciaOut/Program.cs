27d0cf6afd2278394ca0a67ad36cc8791471dafe
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-12 11:10:12 -0300

    Passando argumento por referencia OUT

---
+++ ClassesMetodos/13PassandoArgumentosPorReferenciaOut/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## PASSANDO ARGUMENTOS POR REFERENCIA - OUT - ##\n");
+
+Console.WriteLine("Informe o raio do círculo");
+double raio = Convert.ToDouble(Console.ReadLine());
+
+Circulo circulo = new();
+
+double perimetro = circulo.CalculaAreaPerimetro(raio, out double area);
+
+Console.WriteLine($"Perímetro da circunferência: {perimetro}");
+Console.WriteLine($"Área da circunferência: {area}");
+
+Console.ReadKey();
+
+public class Circulo
+{
+    public double CalculaAreaPerimetro(double raio, out double area)
+    {
+        area = Math.PI * Math.Pow(raio, 2);
+        double perimetro = 2 * Math.PI * raio;
+        return perimetro;
+    }
+}
