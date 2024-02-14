c33a9a5809d21fcc066ae4338d3e25cba2bc63d8
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-14 10:26:14 -0300

    Argumentos NOMEADOS

---
+++ ClassesMetodos/9ArgumentosNomeados/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## ARGUMENTOS NOMEADOS - ##\n");
+
+Email email = new();
+
+//chamando de modo padrão
+Console.WriteLine("chamando de modo padrão, respeitando a ordem dos parâmetro");
+email.Enviar("teste@email.com", "Urgente", "Reunião");
+
+// ARGUMENTOS NOMEADOS
+Console.WriteLine("\n utilizando ARGUMENTOS NOMEADOS");
+email.Enviar(titulo: "Urgente", destino:"Teste@email.com", assunto:"Reunião de orçamento");
+
+
+Console.ReadKey();
+
+
+public class Email
+{
+    public void Enviar(string destino, string titulo, string assunto)
+    {
+        Console.WriteLine($"\n Para {destino} - {titulo} \n Assunto: {assunto}");
+    }
+}
