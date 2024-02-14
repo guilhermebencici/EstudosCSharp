be88dd74bd4a5beae2ea9bf9052ec4ce1aacb3c9
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-14 11:12:54 -0300

    Parâmetros opcionais

---
+++ ClassesMetodos/10ParametrosOpcionais/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("## PARÂMETROS OPCIONAIS- ##\n");
+
+
+Console.WriteLine("Informe o destino");
+var destino = Console.ReadLine();
+
+Console.WriteLine("Informe o titulo");
+var titulo = Console.ReadLine();
+
+Console.WriteLine("Informe o assunto");
+var assunto = Console.ReadLine();
+
+
+Email email = new();
+
+email.Enviar(destino);
+email.Enviar(titulo);
+email.Enviar(destino, titulo, assunto);
+
+Console.ReadKey();
+
+
+public class Email
+{
+    public void Enviar(string destino, string titulo = "Titulo Padrão", string assunto = "Assunto Padrão")
+    {
+        Console.WriteLine($"\n Para {destino} - {titulo} \n Assunto: {assunto}");
+    }
+}
