abd97ada3b6d872abf453e1a1c4ba37e50e8b308
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-15 07:52:16 -0300

    Construtor estático

---
+++ ClassesMetodos/15ConstrutorEstatico/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+public class Pessoa
+{
+    public static int IdadeMinima;
+
+    public int Idade { get; set; }
+    public string Nome { get; set; } = string.Empty;
+
+    public Pessoa(int idade, string nome)
+    {
+        Console.WriteLine("\nExecutando  o construtor parametrizado");
+        Idade = idade;
+        Nome = nome;
+
+    }
+
+    public Pessoa()
+    { }
+
+    // CONSTRUTOR ESTÁTICO
+    static Pessoa()
+    {
+        Console.WriteLine("\nExecutando  o construtor ESTÁTICO ");
+        Console.WriteLine("\nInicializando o campo IdadeMinima");
+        IdadeMinima = 18;
+
+    }
+}
---
+++ ClassesMetodos/15ConstrutorEstatico/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("## CONSTRUTOR ESTÁTICO ##\n");
+
+// INVOCA O CONSTRUTOR PARAMETRIZADO E  O ESTÁTICO
+Pessoa p1 = new(19, "Maria");
+
+Console.WriteLine($"{p1.Nome} - {p1.Idade}");
+Console.WriteLine($"Idade Minima: {Pessoa.IdadeMinima}");
+
+// INVOCA SOMENTE O CONSTRUTOR PARAMETRIZADO ( O ESTÁTICO É INVOCADO APENAS UMA VEZ)
+Pessoa p2 = new(20, "João");
+
+Console.WriteLine($"{p2.Nome} - {p2.Idade}");
+Console.WriteLine($"Idade Minima: {Pessoa.IdadeMinima}");
+
+Console.ReadKey();
