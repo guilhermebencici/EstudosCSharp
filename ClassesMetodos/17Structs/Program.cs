68f3f01e303341ef6592f64ede8975d32c9e8a90
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-15 21:08:25 -0300

    Structs

---
+++ ClassesMetodos/17Structs/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## STRUCTS ##");
+
+Cliente cliente = new Cliente();
+cliente.Nome = "Tico";
+cliente.Idade = 20;
+
+
+Console.WriteLine($"{cliente.Nome} - {cliente.Idade}");
+
+
+Console.ReadKey();
+
+public struct Cliente
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+
+    public Cliente(string? nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+}
