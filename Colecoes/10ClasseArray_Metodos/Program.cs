7e7f5a58830096d085ddd286129fc04f19a3d9f5
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-17 11:25:34 -0300

    Classe Array - Métodos: Reverse, Sort, BinarySearch

---
+++ Colecoes/10ClasseArray_Metodos/Program.cs
@@ -0,0 +1,39 @@
+﻿
+string[] nomes = { "Guilherme", "Gui", "Guiguis", "Guilhermão", "Guizin", "Guizão", "Guigui", "Mr. G", "SAG", "Bencici" };
+
+Console.WriteLine("\n\n Exibindo o Array original...");
+ExibeArray(nomes);
+
+Console.WriteLine("\n\n Invertendo a ordem Array...");
+Array.Reverse(nomes);
+ExibeArray(nomes);
+
+
+Console.WriteLine("\n\n Ordenando Array...");
+Array.Sort(nomes);
+ExibeArray(nomes);
+
+Console.WriteLine("\n\n Localizando um item no array");
+Console.WriteLine("Informe o nome");
+string nome = Console.ReadLine();
+
+var indice = Array.BinarySearch(nomes, nome);
+
+if (indice >= 0)
+{
+    Console.WriteLine($"\n{nome} foi encontrado com índice = {indice}");
+}
+else
+{
+    Console.WriteLine($"\n{nome} não foi encontrado");
+}
+
+Console.ReadKey();
+
+static void ExibeArray(string[] nomes)
+{
+    foreach (var nome in nomes)
+    {
+        Console.WriteLine($"{nome} ");
+    }
+}
