1b9804db78524b61765f0800c39a3cf930f5ae0d
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-16 21:27:03 -0300

    Modificadores de Acesso

---
+++ ClassesMetodos/19ModificadoresAcesso1/MinhaStruct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18Enumeration;
+
+internal struct MinhaStruct
+{
+    //COMO NÃO APONTEI NENHUM MODIFICADOR DE ACESSO, TODOS OS CAMPOS CITADOS ABAIXO SERÃO PRIVATE!
+    int MeuCampo;
+    string? MinhaPropriedade { get; set; }
+    void MeuMetodo()
+    {
+        Console.WriteLine("Meu Metodo");
+    }
+}
---
+++ ClassesMetodos/19ModificadoresAcesso1/Program.cs
@@ -0,0 +1,17 @@
+﻿using _18Enumeration;
+
+Console.WriteLine("## MODIFICADORES DE ACESSO ##");
+
+
+// MESMO INSTANCIANDO, NÃO CONSIGO CHAMAR NENHUM MEMBRO, POIS TODOS ESTÃO COMO PRIVATES(PADRÃO)
+MinhaClasse minhaClasse = new MinhaClasse();
+// minhaClasse...
+
+MinhaStruct minhaStruct = new MinhaStruct();
+// minhaStruct...
+
+MinhaEnum minhaEnum = new MinhaEnum();
+// minhaEnum...
+
+
+Console.ReadKey();
