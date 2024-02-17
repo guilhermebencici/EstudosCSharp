e2ddfd2a2124a32d944d749c6ac91d368a0f291a
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-16 22:46:50 -0300

    Tipos Anônimos

---
+++ ClassesMetodos/21TiposAnonimos/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("## TIPOS ANÔNIMOS ##");
+
+var aluno = new
+{
+    Nome = "Gui",
+    Idade = 31
+};
+
+Console.WriteLine(aluno.Nome + " - " + aluno.Idade + " anos");
+
+
+Console.ReadKey();
