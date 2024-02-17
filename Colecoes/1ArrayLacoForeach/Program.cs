abdac2390c1bb1228381ac33522bb909c2549f69
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-17 11:00:33 -0300

    Array com laço FOREACH - Array III

---
+++ Colecoes/1ArrayLacoForeach/Program.cs
@@ -0,0 +1,34 @@
+﻿// declaração do Array
+int[] numeros;
+
+//alocando na memória e inicializando
+numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+// modo padrão
+//string[] nomes = new string[2] { "Guilherme", "Bencici" };
+
+// simplificando a construção de um array
+string[] nomes = { "Guilherme", "Bencici" };
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine($"{numero} ");
+}
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine($"{nome} ");
+}
+
+//for (int i = 0; i < numeros.Length; i++)
+//{
+//    Console.WriteLine($"Elementos de índice {i}: {numeros[i]}");
+//}
+
+//for (int i = 0; i < nomes.Length; i++)
+//{
+//    Console.WriteLine($"Elementos de índice {i}: {nomes[i]}");
+
+//}
+
+Console.ReadKey();
