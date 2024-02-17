de034d8a40d8f18e58abe22a200d76a6a1fd67c3
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-17 10:30:44 -0300

    Inicio de coleções - Array I

---
+++ Colecoes/1Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿// declaração do Array
+int[] numeros;
+
+//alocando na memória e inicializando
+numeros = new int[10] { 1,2,3,4,5,6,7,8,9,10};
+
+// modo padrão
+string[] nomes = new string[2] { "Guilherme", "Bencici" };
+
+// simplificando a construção de um array
+string[] nomes2 = { "Guilherme", "Bencici" };
+
+
+Console.WriteLine(nomes[1]);
+
+
+// DECLARANDO O ARRAY
+int[] numeros1;
+
+// alocando na memória
+numeros1 = new int[3];
+
+numeros1[0] = 1;
+numeros1[1] = 2;
+numeros1[2] = 3;
+
+Console.ReadKey();
