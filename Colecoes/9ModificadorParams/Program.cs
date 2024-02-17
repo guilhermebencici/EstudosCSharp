3ce8e8da3c1d25de7aa7f4b74fbbbfcebb42fa15
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-17 20:24:24 -0300

    Modificador Params

---
+++ Colecoes/9ModificadorParams/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("## \n SOMA DE NUMEROS INTEIROS ##");
+
+// passando os numeros que queremos somar
+
+int[] valores = { 1, 2, 3 };
+
+var resultado = Calcular.Soma(valores);
+Console.WriteLine(resultado);
+
+var resultado2 = Calcular.Soma(10, 20, 30);
+Console.WriteLine(resultado2);
+
+
+Console.ReadKey();
+
+public class Calcular
+{
+    //criando como static pra não precisar criar uma instancia da classe calcular para acesasr o método soma
+    public static int Soma(params int[] numeros) // recebendo um array de inteiros chamado de numeros
+    {
+        int total = 0;
+        foreach (var numero in numeros)
+        {
+            total += numero;
+        }
+        return total;
+    }
+}
