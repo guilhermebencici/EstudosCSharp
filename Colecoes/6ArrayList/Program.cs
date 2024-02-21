076d08850227ecba3f319ba583549d664707af65
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-21 08:00:51 -0300

    ArrayList() - Método add | Método inicializadores de objetos

---
+++ Colecoes/6ArrayList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+//MÉTODO ADD 
+ArrayList lista = new();
+
+lista.Add("Paulo");
+lista.Add(4.5);
+lista.Add(5);
+lista.Add(true);
+lista.Add(" ");
+lista.Add(null);
+
+//MÉTODO Inicializadores de objetos ***
+
+var lista2 = new ArrayList()
+            {
+                "Paulo", 4.5, 5, true, " ", null
+            };
