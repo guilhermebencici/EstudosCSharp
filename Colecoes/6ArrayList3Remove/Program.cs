71a4343f262a2b656e81c6c41b85a4ff174e438c
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-29 09:41:54 -0300

    ArrayList - Métodos: Remove, RemoveAt, RemoveRange

---
+++ Colecoes/6ArrayList3Remove/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+
+var lista = new ArrayList() { "Ana", 5, true, " ", null, 1.1, "zé", 3, 9, 0, null };
+
+// este método eu estou removendo a primeira ocorrência de Null
+lista.Remove(null);
+
+// neste método, eu aponto o índice do elemento que quero remover
+lista.RemoveAt(4);
+
+// neste método, eu escolho o inicio da remoção, através do indice, e a quantidade de elementos que quero remover
+lista.RemoveRange(0, 2);
+
+Console.ReadKey();
