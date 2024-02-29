6198da97238db6d58255a334b3e8ff917f75e5d1
GuilhermeSouza <guilhermebencici@gmail.com>
2024-02-29 10:07:40 -0300

    ArrayList - Ordenar/Remover todos elementos - Métodos Clear() e Sort()

---
+++ Colecoes/6ArrayListOrdenarRemoverTodos/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections;
+
+var list = new ArrayList() { 1, 9, 10, 2, 3, 5, 100 };
+
+//CONTAINS -> VERIFICO SE O ELEMENTO EM QUESTÃO EXISTE NA COLEÇÃO
+var res1 = list.Contains(1);
+
+Console.WriteLine($"Verificando se o elemento do ínice 1 existe! {res1}");
+
+
+Console.WriteLine($"\nArray List Original (sem ordenação) \n");
+
+foreach(var item in list)
+{
+    Console.Write($"{item} ");
+}
+
+//MÉTODO PARA ORDENAR
+list.Sort();
+
+Console.WriteLine($"\nArray List ORDENADO \n");
+foreach(var item in list)
+{
+    Console.Write($"{item} ");
+}
+
+
+Console.ReadKey();
