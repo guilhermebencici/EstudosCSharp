6dff7138d58b50207dd49b47c7e28efa12a8d792
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-14 09:33:27 -0300

    List<T> - Métodos Remove

---
+++ Colecoes/7List-T-MetodosRemove/Program.cs
@@ -0,0 +1,13 @@
+﻿var lista = new List<int>() { 9, 5, 4, 1, 7, 8, 3, 6, 9, 0 };
+
+// REMOVE O ELEMENTO 7
+lista.Remove(7);
+
+// REMOVE O ELEMENTO INDICADO PELO INDICE
+lista.RemoveAt(4);
+
+// REMOVE O ELEMENTO A PARTIR DE UM INDICE
+// NESTE EXEMPLO, QUERO REMOVER 2 ELEMENTOS SEGUDOS, A PARTIR DO INDICE 0
+lista.RemoveRange(0, 2);
+
+Console.ReadKey();
