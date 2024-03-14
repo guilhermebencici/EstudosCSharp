81053e7f9394c176d899a70204ac83b90e3d0823
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-14 09:38:12 -0300

    List<T> - Acessando elementos

---
+++ Colecoes/7List-T-AcessandoElementos/Program.cs
@@ -0,0 +1,11 @@
+﻿var lista = new List<string>() { "Maria", "Ana", "Marcos", "Pedro" };
+
+// ACESSANDO ELEMENTO USANDO  O INDICE DELE.
+
+var primeiroElemento = lista[0];
+var quartoElemento = lista[3];
+
+Console.WriteLine(primeiroElemento);
+Console.WriteLine(quartoElemento);
+
+Console.ReadKey();
