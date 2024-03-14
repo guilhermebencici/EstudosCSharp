ff90742833549cde47772f7d1c33aa08ddee1887
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-14 10:02:10 -0300

    List<T> - Interação for

---
+++ Colecoes/7List-T-InteracaoFor/Program.cs
@@ -0,0 +1,10 @@
+﻿var lista = new List<string>() { "Maria", "Ana", "Marcos", "Pedro" };
+
+// for para percorrer todos os elementos da minha lista
+//caso eu queira uma quantidade especifica, eu altero a condição, ex: i < 3 (ele irá mostrar os 3 elementos
+for(int i = 0; i < lista.Count; i++)
+{
+    Console.WriteLine(lista[i]);
+}
+
+Console.ReadKey();
