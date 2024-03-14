7ca66841ca80cae9d2f9fbaffa164269f3f40450
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-14 10:35:59 -0300

    List<T> - Iteração foreach

---
+++ Colecoes/7List-T-InteracaoForEach/Program.cs
@@ -0,0 +1,13 @@
+﻿var lista = new List<string>() { "Maria", "Ana", "Marcos", "Pedro", "Soares" };
+
+// Utilizando o FOREACH, não precisamos definir: INICIALIZAÇÃO E CONDIÇÃO
+// "var" infere o tipo de cada um dos elementos
+// "in" intera cada elemento da coleção
+// o numero de interações é igual ao total de elementos da coleção
+
+foreach(var item in lista)
+{
+    Console.WriteLine(lista);
+}
+
+Console.ReadKey();
