5cd82c44dc9945887536d1ae3b9c43b39a3f356f
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-19 09:46:23 -0300

    List<T> - M

---
+++ Colecoes/7List-T-MetodosFind/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> frutas = new()
+{
+    "Uva", "Banana", "Pera", "Maça", "Abacate", "Laranja", "Morango"
+};
+
+var fruta1 = frutas.Find(Procura);
+Console.WriteLine(fruta1);
+
+
+Console.ReadKey();
+
+// predicado
+static bool Procura(string item)
+{
+    return item.Contains('n');
+}
