05e0c3d9dc8c33d0c94b95850acddd8ff17eead0
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-20 08:03:44 -0300

    List<T> - Método FIND e FindLast

--- Colecoes/7List-T-MetodosFind/Program.cs
+++ Colecoes/7List-T-MetodosFind/Program.cs
@@ -5,6 +5,11 @@
 
 //expressão Lambda
 var fruta1 = frutas.Find(i => i.Contains('n'));
-Console.WriteLine(fruta1);
+Console.WriteLine($"\n Utilizando o Find: {fruta1}");
+
+// findlast, vai me mostrar a ultima ocorrência
+var fruta2 = frutas.FindLast(i => i.Contains('n'));
+Console.WriteLine($"\n Utilizando o FindLast: {fruta2}");
+ 
 
 Console.ReadKey();
