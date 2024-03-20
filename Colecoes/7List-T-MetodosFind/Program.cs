710114821f0dc832752f582d34a57e1f4de64e19
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-20 08:17:29 -0300

    List<T> - Métodos FindIndex e FindLastIndex

--- Colecoes/7List-T-MetodosFind/Program.cs
+++ Colecoes/7List-T-MetodosFind/Program.cs
@@ -11,5 +11,13 @@ Console.WriteLine($"\n Utilizando o Find: {fruta1}");
 var fruta2 = frutas.FindLast(i => i.Contains('n'));
 Console.WriteLine($"\n Utilizando o FindLast: {fruta2}");
  
+// findIndex retorna o indice e não o elemento em si
+var fruta3 = frutas.FindIndex(i => i.Contains('n'));
+Console.WriteLine($"\n Utilizando o FindIndex: indice: {fruta3} o que equivale ao item = {frutas[fruta3]}");
+
+// FindLastIndex retorna o ultimo indice e não o elemento em si
+var fruta4 = frutas.FindLastIndex(i => i.Contains('n'));
+Console.WriteLine($"\n Utilizando o FindLastIndex: indice: {fruta4} o que equivale ao item = {frutas[fruta4]}");
+
 
 Console.ReadKey();
