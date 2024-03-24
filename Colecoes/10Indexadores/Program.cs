874fd408ff703e135fb04cc9f7530388a46e15bb
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-24 15:40:11 -0300

    Indexadores

---
+++ Colecoes/10Indexadores/Program.cs
@@ -0,0 +1,15 @@
+﻿using _10Indexadores;
+
+Time time = new Time();
+
+//atribuindo time
+time[1] = "Corinthians";
+time[2] = "Palmeiras";
+time[3] = "São Paulo";
+
+//recuperando time
+string timeUm = time[1];
+
+Console.WriteLine($"O time recuperado é o {timeUm}");
+
+Console.ReadKey();
---
+++ Colecoes/10Indexadores/Time.cs
@@ -0,0 +1,27 @@
+﻿namespace _10Indexadores
+{
+    public class Time
+    {
+        // definindo os limtes do array
+        string[] valor = new string[10];
+
+        public string this[int i]
+        {
+            get
+            {
+                if(i >= 0 && i < valor.Length)
+                {
+                    return valor[i]; //retorna o valor
+                }
+                return "erro";
+            }
+            set
+            {
+                if(i >=0 && i < valor.Length)
+                {
+                    valor[i] = value; // atribui valor
+                }
+            }
+        }
+    }
+}
