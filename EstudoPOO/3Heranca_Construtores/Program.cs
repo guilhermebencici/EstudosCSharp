70313ff0e490ab7e4d84dd4905557f7ad6ecff06
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-26 11:02:19 -0300

    POO => Herança - Construtores

---
+++ EstudoPOO/3Heranca_Construtores/Program.cs
@@ -0,0 +1,27 @@
+﻿// instanciando classe aluno (que é a classe derivada) e assim observando o comportamento da ORDEM de chamada dos construtores
+
+Aluno aluno = new("Gui");
+
+
+Console.ReadKey();
+
+class Pessoa
+{
+    public Pessoa()
+    {
+        Console.WriteLine("Primeio é chamado o Construtor da Classe Base ( Pessoa )");
+    }
+}
+
+class Aluno : Pessoa
+{
+    public Aluno()
+    {
+        Console.WriteLine("Segundo é chamado o Construtor da Classe Derivada ( Aluno )");
+    }
+
+    public Aluno(string nome)
+    {
+        Console.WriteLine("Construtor da Classe Base ( Aluno ) COM parâmetro!");
+    }
+}
