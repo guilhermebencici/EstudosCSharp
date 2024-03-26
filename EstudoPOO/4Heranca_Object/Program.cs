f3d1ec6005910113d1d6b366e3fdc5c4c96bcda9
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-26 11:29:21 -0300

    POO => Herança - Classe Object, pai de todas as classes

---
+++ EstudoPOO/4Heranca_Object/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// instanciando a classe para observar o fato dela ser classe derivada da classe Object
+Vazia v = new();
+
+// métodos oriundos da Classe Base ( PAI ) => Classe Object !!
+v.Equals();
+v.ToString();
+v.GetType();
+v.GetHashCode();
+
+Console.ReadKey();
+
+public class Vazia
+{
+    // não declarado nada, deixando a classe vazia
+}
