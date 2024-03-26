a360fd7f31bbc7a8bf1d80e97021db3dc9e35ed6
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-26 11:42:25 -0300

    POO => Herança - Modfiicador New - Reforça a utilização do método da classe derivada

---
+++ EstudoPOO/5Heranca_Modificador_New/Program.cs
@@ -0,0 +1,20 @@
+﻿Aluno a = new();
+a.Nome = "Guilherme";
+a.Curso = "ADS";
+
+Console.WriteLine(a.Saudacao());
+
+Console.ReadKey();
+
+class Pessoa
+{
+    public string? Nome { get; set; }
+    public string Saudacao() => $"Oi, eu sou o(a) {Nome}";
+}
+
+class Aluno : Pessoa
+{
+    public string? Curso { set; get; }
+    // O modificador NEW, reforça a minha intenção de usar o método da classe derivada, mesmo que ele exista na classe Base
+    public new string Saudacao() => $"Oi, eu sou o (a) {Nome}, do curso {Curso}";
+}
