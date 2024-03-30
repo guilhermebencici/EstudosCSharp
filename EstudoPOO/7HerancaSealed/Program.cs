8f64dc1fe3da6366476b65de7547be545ee6885f
GuilhermeSouza <guilhermebencici@gmail.com>
2024-03-29 21:41:18 -0300

    POO => Herança - SEALED na classe

---
+++ EstudoPOO/7HerancaSealed/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.ReadKey();
+
+class Pessoa
+{
+    public string? Nome { get; set; }
+    public void ExibeNome()
+    {
+        Console.WriteLine($"Meu nome é {Nome}");
+    }
+}
+
+class Cliente : Pessoa
+{
+    public new void ExibeNome()
+    {
+        Console.WriteLine($"\n Nome do Cliente: {Nome}");
+    }
+}
