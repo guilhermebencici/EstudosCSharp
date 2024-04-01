d9990e1829e0e155e48ce6c9656de2c3268d6853
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-01 08:51:21 -0300

    POO - Exemplo de uma conversão de tipo mal sucedida, gerando uma exception

---
+++ EstudoPOO/10Heranca_is_as/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Object carroObj = new Carro();
+
+// fazendo a conversão explícita, sem usar o operador 'as'
+
+try
+{
+	var x = (string)carroObj; // exception
+}
+catch (Exception ex)
+{
+
+	Console.WriteLine("\nA Conversão falhou...\n");
+	Console.WriteLine(ex.Message);
+}
+
+Console.ReadKey();
+
+class Carro { }
