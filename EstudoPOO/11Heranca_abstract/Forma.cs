1fa3a77e3c1d4935938c03d24df6fdea3b979cb6
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-09 17:03:50 -0300

    POO - Heranca_Abstrata

---
+++ EstudoPOO/11Heranca_abstract/Forma.cs
@@ -0,0 +1,19 @@
+﻿namespace _11Heranca_abstract
+{
+    public abstract class Forma
+    {
+        public string? Cor { get; set; }
+        public double Area { get; set; }
+        public double Perimetro { get; set; }
+
+        // MÉTODOS ABSTRATOS
+        public abstract void CalcularArea();
+        public abstract void CalcularPerimetro();
+
+        // MÉTODOS COMUM
+        public string Descricao()
+        {
+            return "Sou a classe abstrata Forma";
+        }
+    }
+}
---
+++ EstudoPOO/11Heranca_abstract/Program.cs
@@ -0,0 +1,21 @@
+﻿using _11Heranca_abstract;
+
+Quadrado q = new();
+Console.WriteLine(q.Descricao());
+
+Console.WriteLine("\n Informe a cor da figura");
+q.Cor = Console.ReadLine();
+
+Console.WriteLine("\n Informe o valor do lado do quadrado");
+q.Lado = Convert.ToInt32(Console.ReadLine());
+
+q.CalcularArea();
+q.CalcularPerimetro();
+
+Console.WriteLine($"\nÁrea do quadrado: {q.Area} m2");
+Console.WriteLine($"\nPerímetro do quadrado: {q.Perimetro} m");
+
+
+Console.WriteLine($"\nO quadrado tem a cor : {q.Cor} ");
+
+Console.ReadKey();
