9a45a6a0249e4c7b35f9f091a5fd15dfeaa7eb83
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-09 23:41:36 -0300

    POO - Agregação, conceito!

---
+++ EstudoPOO/16Agregacao/Departamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16Agregacao
+{
+    public class Departamento
+    {
+        public Departamento(string? nome)
+        {
+            Nome = nome;
+            professores = new List<Professor>();
+        }
+
+        public string? Nome { get; set; }
+        private List<Professor>? professores { get; set; }
+
+        public void IncluirProffessor(Professor professor)
+        {
+            professores?.Add(professor);
+        }
+
+        public void ListaProfessores()
+        {
+            Console.WriteLine($"\nDepartamento de : {Nome} \n");
+
+            foreach (var prof in professores)
+                Console.WriteLine(prof.Nome + " -> " + prof.Disciplina);
+        }
+    }
+
+
+}
---
+++ EstudoPOO/16Agregacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using _16Agregacao;
+
+Professor prof1 = new Professor("Carlos", "Química");
+Professor prof2 = new Professor("Maria", "Inglês");
+Professor prof3 = new Professor("Ana", "Literatura");
+Professor prof4 = new Professor("Paulo", "Matemática");
+
+Departamento dep1 = new Departamento("Exatas");
+dep1.IncluirProffessor(prof1);
+dep1.IncluirProffessor(prof4);
+dep1.ListaProfessores();
+
+
+Departamento dep2 = new Departamento("Huamanas");
+dep2.IncluirProffessor(prof2);
+dep2.IncluirProffessor(prof3);
+dep2.ListaProfessores();
+
+Console.ReadKey();
