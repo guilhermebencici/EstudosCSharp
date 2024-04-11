e75918ba416dd77f156a7516b8df77b8b03e5aa4
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-11 13:59:03 -0300

    GENERICS -> Equals, exemplo de comparação com tipos primitivos e com tipos especiais

---
+++ Generics_ColecoesGenericas/061GetHashCode_Equals/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+string a = "Paulo";
+string b = "Paulo";
+
+// IRÁ RETORNAR TRUE, PORQUE PARA TIPOS PRIMITIVOS, O EQUALS COMPARA O VALOR DO OBJETO!
+int x = 100;
+int y = 100;
+
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(x.Equals(y));
+
+
+//O RESULTADO SERÁ FALSE, PORQUE PESSOA 1 E PESSOA 2 NÃO SÃO O MESMO OBJETO, SÃO REFRENCIAS.
+
+var pessoa1 = new Pessoa(1, "Gui");
+var pessoa2 = new Pessoa(1, "Gui");
+
+Console.WriteLine(pessoa1.Equals(pessoa2));
+
+Console.ReadKey();
+
+
+public class Pessoa
+{
+    public Pessoa(int id, string? nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+}
