56b6a652ef27431d9b411f6b4ac917d0b82a386b
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-15 13:50:39 -0300

    GENERICS -> Stack, criação com os 3 tipos de construtores

---
+++ Generics_ColecoesGenericas/13Stack/Program.cs
@@ -0,0 +1,21 @@
+﻿Stack<int> numeros = new Stack<int>();
+numeros.Push(1);
+
+int[] array1 = new int[] { 1, 2, 3, 5 };
+Stack<int> pilha1 = new Stack<int>(array1);
+
+var impares = new Stack<int>(3);
+impares.Push(1);
+
+/*
+
+    MÉTODOS
+
+PUSH() -> Inclui eleento no topo da pilha
+POP() -> Remove e retornar um elemento do topo da pilha
+PEEK() -> Retorna o elemento do topo da pilhar sem removê-lo
+CONTAINS() -> Verifica se existe um elemento na pilha (true or false)
+CLEAR() -> Remove todos os elementos da pilha.
+TOARRAY() -> Copia a pilha em um novo array unidimensional
+
+*/
