4d6e91ae5d2af5c313853236a0c21d9bcff9e9eb
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-15 14:26:20 -0300

    GENERICS -> Stack, exemplos da utilização de alguns métodos, como Pop, contains e ainda alguns IFs

---
+++ Generics_ColecoesGenericas/14StackUndo/EditorText.cs
@@ -0,0 +1,27 @@
+﻿namespace _14StackUndo
+{
+    public class EditorText
+    {
+        private Stack<char> undoStack = new Stack<char>();
+        private string texto = "";
+
+        public void DigitarChar(char c)
+        {
+            texto += c;
+            undoStack.Push(c);
+            Console.Write($"Texto: {texto} ");
+        }
+
+        public void Undo()
+        {
+            if(undoStack.Count > 0)
+            {
+                char ultimoChar = undoStack.Pop();
+                texto = texto.Substring(0,texto.Length - 1);
+                Console.WriteLine($"texto: {texto}");
+            }
+        }
+    }
+
+
+}
