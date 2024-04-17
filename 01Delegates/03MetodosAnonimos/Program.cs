9c5000afc9a72fed0e2d1472cafe23a2c5623bd6
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-17 08:50:17 -0300

    Método Anonimo com delegate, exeplo simples

---
+++ 01Delegates/03MetodosAnonimos/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Imprimir imprimir = delegate (int valor)
+{
+    Console.WriteLine($"Método Anônimo : {valor}");
+};
+
+imprimir(100);
+imprimir(200);
+imprimir(300);
+
+Console.ReadKey();
+
+// declarando o delegate
+public delegate void Imprimir(int valor);
