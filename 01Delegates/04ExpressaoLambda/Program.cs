9d12e988bd0b31a6db54875b88f13c11f87b4944
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-18 17:38:16 -0300

    Expressão lambda, exemplo

---
+++ 01Delegates/04ExpressaoLambda/Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> nomes = new List<string>();
+nomes.Add("Maria");
+nomes.Add("Miriam");
+nomes.Add("Paulo");
+nomes.Add("Carlos");
+
+//string resultado = nomes.Find(VerificaNomeNaLista);
+string resultado = nomes.Find(nome => nome.Equals("Paulo"));// expressão lambda
+
+Console.WriteLine(resultado);
+
+Console.ReadKey();
