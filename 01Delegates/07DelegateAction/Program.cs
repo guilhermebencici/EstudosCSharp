67bf73da909fd1cf6ca57efb9d1858c55546ad0b
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-19 07:27:06 -0300

    delegate Action

---
+++ 01Delegates/07DelegateAction/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.WriteLine("Informe um número inteiro :");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+//delegateACtion + método Anônimo + operador lambda
+Action<int> dobra = x => Console.WriteLine(x * 2);
+dobra(numero);
+
+Console.ReadKey();
+
