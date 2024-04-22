dcaa053fb36843873a02b4bb711ed913c5b9fe1c
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-22 09:16:39 -0300

    exemplo de linq - Sitaxe de consulta e Sintaxe de método -

---
+++ 01Delegates/11Linq1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+List<string> nomes = new List<string>()
+{
+    "Ana", "Maria", "Pedro", "Lair", "Hugo"
+};
+
+//LINQ -> SINTAXE DE CONSULTA
+var resultado = (from m in nomes
+                 where m.Contains('o')
+                 select m).ToArray();
+
+ExibirResultado(resultado);
+
+// LINQ -> SINTAXE DE MÉTODO 
+
+// ToList() -> Converte um IEnumerable<T> para um List<T>
+// ToArray() -> Converte um IEnumerable<T> para um Array<T>
+var resultado1 = nomes.Where(n => n.Contains('a')).ToList();
+
+ExibirResultado(resultado1);
+
+Console.ReadKey();
+
+static void ExibirResultado(IEnumerable<string> resultado)
+{
+    foreach (var item in resultado)
+        Console.WriteLine(item);
+}
