110f7b11b41a888c936aa1a8c2610fbf61d566db
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-26 10:45:10 -0300

    Exemplo de FileStream utilizando Bloco Using

---
+++ ArquivosDiretoriosStreams/07FileStreamBlocoUsing/Program.cs
@@ -0,0 +1,21 @@
+﻿string caminhoArquivo = @"d:\dados\poesia.txt";
+
+try
+{
+    // FORMA DECLARATIVA DO BLOCO USING
+
+    //using FileStream fs = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+    using StreamReader leitor = File.OpenText(caminhoArquivo);//new StreamReader(fs);
+
+    string? linha;
+    while ((linha = leitor.ReadLine()) != null)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadKey();
