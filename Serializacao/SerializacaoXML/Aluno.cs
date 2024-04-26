1edfaa23c136090aff74ad78e2f901477f954929
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-26 13:48:36 -0300

    Serialização: XML

---
+++ Serializacao/SerializacaoXML/Aluno.cs
@@ -0,0 +1,20 @@
+﻿namespace SerializacaoXML;
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int Idade;
+
+    // por ser XML, precisa ter o construtor padrão (Vazio)
+    public Aluno()
+    {}
+    public Aluno(int id, string name, string email, int idade)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Idade = idade;
+    }
+}
