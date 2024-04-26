4b99d06a69f0ffac33d97ddf4260a92e1e23a4b9
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-26 13:59:18 -0300

    Serialização: JSON

---
+++ Serializacao/SerializacaoJSON/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SerializacaoJSON;
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    [JsonIgnore]// desta forma, esta propriedade Idade não será serializada
+    public int Idade { get; set; }
+    public Aluno()
+    {
+    }
+    public Aluno(int id, string name, string email, int idade)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Idade = idade;
+    }
+}
---
+++ Serializacao/SerializacaoJSON/Program.cs
@@ -0,0 +1,16 @@
+﻿using SerializacaoJSON;
+using System.Text.Json;
+
+Aluno aluno1 = new Aluno(10, "Maria", "maria@yahoo.com", 17);
+
+var caminhoArquivo = @"d:\dados\Serializados\AlunoSerializado.json";
+
+using (FileStream stream = new FileStream(caminhoArquivo,
+                                          FileMode.OpenOrCreate, FileAccess.ReadWrite))
+{
+    JsonSerializer.Serialize(stream, aluno1);
+}
+
+Console.WriteLine("Objeto serializado para JSON com sucesso!");
+
+Console.ReadKey();
