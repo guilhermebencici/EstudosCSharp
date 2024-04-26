d6f8af059b5b4b3ab4a88e88bb2be1a651260339
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-26 13:53:50 -0300

    Desserialização: XML

---
+++ Serializacao/DesserializacaoXML/Aluno.cs
@@ -0,0 +1,20 @@
+﻿namespace DesserializacaoXML;
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int Idade;
+
+    public Aluno()
+    {
+    }
+    public Aluno(int id, string name, string email, int idade)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Idade = idade;
+    }
+}
---
+++ Serializacao/DesserializacaoXML/Program.cs
@@ -0,0 +1,16 @@
+﻿using DesserializacaoXML;
+using System.Xml.Serialization;
+
+string caminhoArquivo = @"d:\dados\Serializados\AlunoSerializado.xml";
+
+XmlSerializer serializer = new XmlSerializer(typeof(Aluno));
+
+using (StreamReader reader = new StreamReader(caminhoArquivo))
+{
+    var aluno = (Aluno)serializer.Deserialize(reader);
+
+    Console.WriteLine($"Aluno XML desserializado - Id: {aluno.Id}, Nome: {aluno.Name}, " +
+                      $"Email: {aluno.Email}, Idade: {aluno.Idade}");
+}
+
+Console.ReadKey();
