0ff9dde7012fd7b723d6cba72b55c21f5b0cc3e0
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-26 14:04:21 -0300

    Desserialização: JSON

---
+++ Serializacao/DesserializacaoJSON/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesserializacaoJSON;
+using System.Text.Json;
+
+var caminhoArquivo = @"c:\dados\Serializados\AlunoSerializado1.json";
+
+// Lê todo o conteúdo do arquivo JSON
+string jsonContent = File.ReadAllText(caminhoArquivo);
+
+var aluno = JsonSerializer.Deserialize<Aluno>(jsonContent);
+
+Console.WriteLine($"Aluno desserializado - ID: {aluno.Id}, Nome: {aluno.Name}, " +
+                  $"Email: {aluno.Email}, Idade: {aluno.Idade}");
+
+Console.ReadKey();
