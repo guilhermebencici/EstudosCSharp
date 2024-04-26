57c7a97e42c7cdf156eb1e3e4e41137acb776f96
GuilhermeSouza <guilhermebencici@gmail.com>
2024-04-26 08:51:44 -0300

    Utilizando a classe Directory: Criando diretórios

---
+++ ArquivosDiretoriosStreams/03Directory1/Program.cs
@@ -0,0 +1,25 @@
+﻿// meu diretório
+var caminhoDiretorio = @"d:\dados\MeuDiretorio";
+string caminhoDestino = @"d:\dados\MeuDiretorioNovo";
+
+Console.WriteLine("\nCriando um novo diretório");
+
+try
+{   //sempre validar se o tal diretório existe!
+    if (Directory.Exists(caminhoDiretorio))
+    {
+        Directory.CreateDirectory(caminhoDiretorio);
+        Console.WriteLine($"Diretório {caminhoDiretorio} criado com sucesso!");
+    }
+    else
+    {
+        Console.WriteLine($"O diretório  {caminhoDiretorio} já existe!");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    //Console.WriteLine(ex.StackTrace);
+}
+
+Console.ReadKey();
